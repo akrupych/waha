98ae7ebaebf601f845ac56b46eaade2087b52610
Andriy Krupych <andriy.krupych@navico.com>
2018-11-18 02:01:51 +0200

    firebase db

--- app/src/main/java/akrupych/waha/MainActivity.kt
+++ app/src/main/java/akrupych/waha/MainActivity.kt
@@ -2,11 +2,23 @@ package akrupych.waha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val database = FirebaseDatabase.getInstance()
+        val units = database.getReference("Units")
+        units.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(data: DataSnapshot) {
+                helloTextView.text = data.value.toString()
+            }
+            override fun onCancelled(error: DatabaseError) {
+                error.toException().printStackTrace()
+            }
+        })
     }
 }
